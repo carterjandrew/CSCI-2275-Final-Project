b3b4ff0a1b8be6ee26aa1003e0caba0ae0c4664a
Carter Andrew <52466114+carterjandrew@users.noreply.github.com>
2020-11-28 20:25:34 -0700

    Add files via upload

---
+++ Graph.h
@@ -0,0 +1,35 @@
+#ifndef MAZE_H
+#define MAZE_H
+#include <vector>
+#include <iostream>
+//The graph is designed using a maze node so that we can dynamically increase the size
+//For our nodes we will limit the maximum connections to 4 in order to keep our maze easily visually representable
+struct MazeNode
+{
+    public:
+    //int weight = 1;
+    bool wall = false;
+    MazeNode* up = nullptr;
+    MazeNode* down = nullptr;
+    MazeNode* left = nullptr;
+    MazeNode* right = nullptr;
+    MazeNode(bool w);
+    MazeNode(bool w, MazeNode* u, MazeNode* d, MazeNode* l, MazeNode* r);
+};
+class Maze{
+    private:
+    int width;
+    int height;
+    MazeNode* topLeft = nullptr;
+    MazeNode* topRight = nullptr;
+    MazeNode* botLeft = nullptr;
+    MazeNode* botRight = nullptr;
+    public:
+    void print();
+    Maze(int w, int h);
+    void updateSize(int w, int h);
+    void recursiveDivision();
+    void findPath(int sx, int sy, int ex, int ey);
+    int probeDepth(MazeNode* start, int dir);
+};
+#endif
